9162be4f305c1318bc35b03b0124b01e1daa5d94
HaiPDV <141106097+hainqm@users.noreply.github.com>
2024-11-24 17:04:39 +0700

    Add files via upload
    add to cart

--- admin/admin.php
+++ admin/admin.php
@@ -1,5 +1,32 @@
-    <h2>Đây là trang ADMIN</h2>
+<?php
+session_start();
 
+// Kiểm tra xem người dùng đã đăng nhập và có vai trò admin hay không
+if (!isset($_SESSION['username']) || $_SESSION['role'] != '1') {
+    header("Location: login.php"); // Chuyển hướng về trang đăng nhập nếu không phải admin
+    exit();
+}
 
+// Nội dung của trang admin
+    $hi_admin = "Xin chào boss: " . $_SESSION['username'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Admin</title>
+</head>
+
+<body>
+    <h1> <?php 
+        echo $hi_admin;
+    ?></h1>
+    <a href="../logout.php" style="text-decoration: none">
+        <h3>Đăng xuất</h3>
+    </a>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
+session_start();
+
+// Kiểm tra nếu chưa đăng nhập thì chuyển hướng đến login.php
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+};
+    include "./model/pdo.php";
+    include "./model/model_products.php";
+    include "./model/model_categories.php";
     include "./view/header.php";
-    if(isset($_GET['act']) && ($_GET['act']!="")){
+    include "./global.php";
+
+
+
+     //kiểm tra xem mảng có tồn tại không
+    if(!isset($_SESSION["carts"])){
+        $_SESSION["carts"] = [];
+    }
+
+    
+    $load_categories = list_all_categories_home();
+    $load_products = list_all_products_home();
+    if(isset($_GET['act']) && ($_GET['act']!="") ){
         $act = $_GET['act'];
         switch ($act) {
             case 'home':
@@ -15,6 +37,44 @@
             case 'about':
                 include "./view/about.php"; 
                 break;
+            case 'detail':
+                if(isset($_GET["product_id"]) && $_GET["product_id"]>0){
+                    $product_id = $_GET["product_id"];
+                    $detail = load_one_products($product_id);
+                    include "./view/detail.php"; 
+                }
+                // else{
+                //     include "./view/product.php";
+
+                // }
+                 break;
+
+            //thêm vào giỏ hàng
+             case 'cart':
+                if(isset($_POST["addtocart"]) && $_POST["addtocart"]){
+                    $product_id = $_POST["product_id"];
+                    $product_name = $_POST["product_name"];
+                    $price = $_POST["price"];
+                    $image_url = $_POST["image_url"];
+                    $quantity = 1;
+                    $total = $quantity * $price;
+
+                    $cart_product =[$product_id,$product_name,$price,$image_url,$quantity,$total];
+                    array_push($_SESSION["carts"],$cart_product);
+                }
+                include "./view/carts/add_to_cart.php"; 
+                break;
+                
+                case 'delete_cart': 
+                    if(isset($_GET["id_cart"])){
+                        array_slice($_SESSION["carts"],$_GET["id_cart"],1);
+                    }else{
+                        $_SESSION["carts"] = [];
+                    }
+                    header('Location:./index.php?act=cart');
+                    
+                    
+                    break;
             default:
                 include "./view/home.php";
                 break;
